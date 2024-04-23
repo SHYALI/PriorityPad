8f920ef309bfb29c50e4a6ae5665704f98f03e00
shyali <shyali.narayan2002@gmail.com>
2024-04-23 18:29:08 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
+const cors = require('cors')
 
 
 mongoose.connect(process.env.MONGODB_URL);
@@ -15,6 +16,8 @@ const todoSchema = new mongoose.Schema({
 const Todo = mongoose.model("Todo", todoSchema);
 
 app.use(express.json());
+app.use(cors())
+
 
 
 app.get("/", async (req, res) => {
